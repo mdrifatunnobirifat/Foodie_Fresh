c318c49e54309c3495a429d309f4a5acc0320f18
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-16 20:25:12 +0600

    :art: Style: Fix the WhoweAre component design of  FoodieFresh

--- app/components/WhoweAre.tsx
+++ app/components/WhoweAre.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import WhoweCard from "./WhoweCard";
 
 export default function WhoweAre() {
-const items=[
+const Item=[
     {heading:"Real Product", para:"mdj smdj msdj msdjf sdmjf"},
     {heading:"Real Product", para:"mdj smdj msdj msdjf sdmjf"},
     {heading:"Real Product", para:"mdj smdj msdj msdjf sdmjf"}
@@ -10,13 +10,14 @@ const items=[
 ]
 
   return (
-    <div className="flex flex-row items-center px-4 py-4 max-w-full">
+    <div className="flex flex-col lg:flex-row items-center justify-center  py-10 gap-10">
 {/* Left*/}
-      <div className=" w-full lg:w-1/2">
-        <h1 className="text-3xl font-bold text-emerald-950">Who We Are &<br/>What We Do</h1>
+      <div className=" flex flex-col w-full lg:w-1/2  items-center lg:items-baseline-last text-center lg:text-center  ">
+        <h1 className="text-3xl  lg:text-4xl font-bold text-emerald-950">Who We Are &<br/>What We Do</h1>
         <p className="mt-1 text-gray-700">Welcome to our delivery service.<br/>Welcome to our delivery service.</p>
-         <div className="space-y-4">
-            {items.map((item)=>(
+         <div className="flex flex-col items-center lg:items-start space-y-4 max-w-sm md:max-w-lg mx-auto lg:mx-0 w-full">
+
+            {Item.map((item)=>(
                 <WhoweCard
                    h={item.heading}
                    p={item.para}
@@ -24,13 +25,11 @@ const items=[
             ))}
 
          </div>
-        <div>
-
-        </div>
+        
       </div>
 
 {/* Right*/}
-      <div className=" hidden lg:block lg:w-1/2 relative h-[300px] md:h-[500px]"> 
+      <div className="hidden lg:block lg:w-1/2 relative h-[300px] md:h-[500px] w-full"> 
         <Image src="/vecteezy_fast-delivery-by-scooter-on-mobile-e-commerce-concept_17732847.jpg" alt="Delivery scooter icon"fill
           className="object-contain" />
       </div>
--- app/components/WhoweCard.tsx
+++ app/components/WhoweCard.tsx
@@ -7,17 +7,19 @@ interface CardProps {
 export default function WhoweCard({h,p}:CardProps)
 {
     return (
-    <div className="flex items-center bg-[#c8ef9b] hover:bg-[#23803a] p-2 rounded-xl cursor-pointer transition-all group w-1/2 ">
+    <div className="flex items-center bg-[#dbfae0] hover:bg-[#23803a] p-2 rounded-xl cursor-pointer transition-all group w-full max-w-md">
       
-      <div className="bg-white p-2 rounded-full border-b-2">
-        <Image src="/food-svgrepo-com.svg" alt="icon" width={22} height={22} />
+      <div className="bg-white h-12 w-12 rounded-full flex items-center justify-center overflow-hidden p-1 shadow-sm">
+        <Image src="/food-svgrepo-com.svg" alt="icon" width={40} height={40} 
+        className="object-contain"/>
       </div>
-      <div className="ml-10 flex-1">
+      <div className=" flex-1">
         <h3 className="font-bold text-emerald-950 group-hover:text-white">{h}</h3>
         <p className="text-sm group-hover:text-white/80">{p}</p>
       </div>
       <div className="bg-emerald-700 p-2 rounded-full group-hover:bg-white">
-        <Image src="/right-arrow-svgrepo-com.svg" alt="arrow" width={13} height={13} />
+        <Image src="/right-arrow-svgrepo-com.svg" alt="arrow" width={15} height={15} 
+        className="object-contain"/>
       </div>
       
 
