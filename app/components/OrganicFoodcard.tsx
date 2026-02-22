519e24562d27f90af5d353d5153af61834e49caa
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-02-22 15:14:49 +0600

    :art: Fix: Added padding in x axix for the OrganicFoodCrad.tsx

--- app/components/OrganicFoodcard.tsx
+++ app/components/OrganicFoodcard.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 export default function OrganicFoodcard({h}:CardProps)
 {
     return (
-    <div className="flex items-center  py-1 lg:py-3 ">
+    <div className="flex items-center   py-1 lg:py-3 ">
       
      <div className="flex items-center justify-center  w-5 lg:w-6 h-5 lg:h-6 rounded-full bg-emerald-700  group-hover:bg-white transition-colors">
         <Image 
@@ -17,7 +17,7 @@ export default function OrganicFoodcard({h}:CardProps)
           className="object-contain invert group-hover:invert-0" 
         />
       </div>
-      <div className=" flex-1">
+      <div className="  px-1 flex-1">
         <h5 className="font-bold text-xs lg:text-base text-emerald-950 group-hover:text-white">{h}</h5>
       </div>
     </div>
